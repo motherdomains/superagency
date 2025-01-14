913303f677a8edadcf95bacd34902b9200239add
motherdomains <sea@mother.domains>
2025-01-14 09:51:05 +0530

    Daily update: 2025-01-14

--- blueprints/song_contest/views.py
+++ blueprints/song_contest/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from blueprints.song_contest.models import SongCountry, SongShow
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from blueprints.song_contest.models import SongCountry, SongShow, SongShowCountry
 from blueprints.song_contest.forms import CountryForm, SongShowForm
 from flask_admin.contrib.sqla import ModelView
 from extensions import db
@@ -29,16 +29,43 @@ def register_admin_views(admin):
 
 # Routes for Song Contest
 
-# Route for listing all shows
-#@song_contest_bp.route('/')
-#def show_list():
-#    """Render a list of song shows."""
-#    shows = SongShow.query.order_by(SongShow.showDate.desc()).all()
-#    return render_template('show_list.html', shows=shows)  # Templates are now in the main #'templates' folder
-
 # Route for listing all countries
 @song_contest_bp.route('/countries', endpoint='country_list')
 def country_list():
     """Render a list of countries."""
     countries = SongCountry.query.order_by(SongCountry.display_order.asc()).all()
     return render_template('country_list.html', countries=countries)  # Templates are now in the main 'templates' folder
+
+# Route for adding countries to a show
+@song_contest_bp.route('/shows/<int:show_id>/add-countries', methods=['GET', 'POST'], endpoint='add_countries')
+def add_countries(show_id):
+    """Add countries to a specific show."""
+    show = SongShow.query.get_or_404(show_id)
+    countries = SongCountry.query.order_by(SongCountry.country.asc()).all()
+    total_contestants = show.totalContestants
+    selected_countries = []
+
+    if request.method == 'POST':
+        selected_ids = request.form.getlist('countries')
+        if len(selected_ids) != total_contestants:
+            flash(f"Please select exactly {total_contestants} countries.", "warning")
+        else:
+            # Clear any existing countries for this show
+            SongShowCountry.query.filter_by(showID=show.id).delete()
+
+            # Add the selected countries to the show
+            for country_id in selected_ids:
+                show_country = SongShowCountry(showID=show.id, countryID=int(country_id))
+                db.session.add(show_country)
+            db.session.commit()
+
+            flash(f"{len(selected_ids)} countries added to the show successfully!", "success")
+            return redirect(url_for('song_contest.add_countries', show_id=show_id))
+
+    return render_template(
+        'add_countries.html',
+        show=show,
+        countries=countries,
+        selected_countries=selected_countries,
+        total_contestants=total_contestants,
+    )
